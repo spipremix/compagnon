67df28ac9214b07fc092b59fb748e85d882b42a8
salvatore <salvatore@rezo.net>
2012-01-09 07:55:22 +0000

    langues (compagnon)

---
+++ lang/compagnon_sk.php
@@ -0,0 +1,75 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'c_accueil_bienvenue' => 'Vitajte, @nom@!',
+	'c_accueil_configurer_site' => 'Nastaviť si stránku',
+	'c_accueil_configurer_site_texte' => 'Jednou z prvých vecí, ktoré treba urobiť, je dať stránke názov. 
+                 Momentálne má názov "@nom@". Názov stránky sa zobrazuje v hornej časti tejto stránky. 
+                Kliknutím môžete zmeniť jej názov a tiež pridať logo a slogan.',
+	'c_accueil_publication' => 'Publikujte!',
+	'c_accueil_publication_texte' => 'Na publikovanie stránky musíte vytvoriť článok.
+		Môžete to urobiť v menu "Upraviť stránku" kliknutím na "Rubriky".',
+	'c_accueil_texte' => 'Práve ste sa dostali do súkromnej zóny SPIPu.',
+	'c_accueil_texte_revenir' => '		Táto stránka uvádza najnovšiu redakčnú aktivitu na vašej stránke.
+		Túto stránku môžete navštíviť kedykoľvek kliknutím na ikonu domčeka 
+                            na vrchu pod svojím menom.',
+	'c_article_redaction' => 'Článok sa upravuje',
+	'c_article_redaction_redacteur' => 'Článok sa upravuje',
+	'c_article_redaction_redacteur_texte' => 'Pour proposer votre article aux administrateurs du site et aux autres rédacteurs,
+		dans le cadre sur le côté, changez « en cours de rédaction » par « proposé à la publication ».', # NEW
+	'c_article_redaction_texte' => 'Pour publier le contenu de cet article sur le site public, il faut changer son statut.
+		Dans le cadre sur le côté, changez « en cours de rédaction » par « publié en ligne ».', # NEW
+	'c_articles_creer' => 'Comment créer un article ?', # NEW
+	'c_articles_creer_texte' => 'Vous ne pourrez créer un article depuis cette page
+		qu\'à partir du moment où il existe une rubrique dans votre site.
+		Vous pouvez la créer depuis le menu « Édition », puis « Rubriques ».', # NEW
+	'c_job' => 'Čo treba urobiť…',
+	'c_job_texte' => 'Cette page liste les prochaines tâches de maintenance
+		que doit effectuéer SPIP. Ces tâches sont exécutées à interval régulier
+		ou ponctuellement pour des traitements lourds demandés par des plugins,
+		tel que l\'envoi d\'email en masse.', # NEW
+	'c_rubrique_publier' => 'Vytvorte článok',
+	'c_rubrique_publier_texte' => 'Une rubrique n\'est visible sur le site public
+		qu\'à partir du moment où elle contient au moins un contenu publié dedans. Par exemple, un article publié.
+		Créez donc un article. Vous pouvez le faire depuis cette page, sous le descriptif de votre rubrique.', # NEW
+	'c_rubriques_creer' => 'Vytvorte prvú rubriku!',
+	'c_rubriques_creer_texte' => 'Rubriky tvoria základnú štruktúru stránky.
+		V nich môžete vytvárať články.
+		Začnite vytvárať prvú rubriku.',
+
+	// E
+	'explication_activer_compagnon' => 'Asistent pridáva komentáre na niektoré stránky
+			súkromnej zóny na pomoc s ovládaním SPIPu. Chcete ho aktivovať?',
+	'explication_reinitialiser_compagnon' => 'Správy, ktoré autor videl, sa už viac nezobrazujú.			Chcete obnoviť tieto správy?',
+
+	// L
+	'label_activer_compagnon' => 'Aktivovať Sprievodcu?',
+	'label_reinitialiser_compagnon' => 'Obnoviť správy zo Sprievodcu?',
+
+	// O
+	'ok' => 'OK',
+	'ok_bien' => 'Dobre!',
+	'ok_jai_compris' => 'Porozumel(a) som!',
+	'ok_merci' => 'Ďakujem',
+	'ok_parfait' => 'Dokonalé!',
+
+	// R
+	'reinitialisation' => 'Obnovenie',
+	'reinitialisation_ok' => 'Vykonáva sa obnovenie.',
+	'reinitialiser' => 'Obnoviť',
+	'reinitialiser_moi' => 'Áno, len tie, ktoré som už prečítal(a)',
+	'reinitialiser_tous' => 'Áno, ktokoľvek je autor',
+
+	// T
+	'titre_compagnon' => 'Sprievodca',
+	'titre_page_configurer_compagnon' => 'Nastaviť Sprievodcu'
+);
+
+?>
