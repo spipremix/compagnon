f65ab94d6b102177dcf8b574822396c084893d3e
george <george@middleeastwatch.net>
2012-01-16 09:53:31 +0000

    langues (compagnon)

---
+++ lang/compagnon_en.php
@@ -0,0 +1,76 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.org
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'c_accueil_bienvenue' => 'Welcome@nom@ !',
+	'c_accueil_configurer_site' => 'Configure your site',
+	'c_accueil_configurer_site_texte' => 'Une des premières choses à faire est de donner un nom à votre site.
+		Il se nomme actuellement « @nom@ ». Le nom du site est affiché tout en haut de cette page.
+		En cliquant dessus, vous pourrez modifier son nom, mais aussi lui donner un logo et un slogan.', # NEW
+	'c_accueil_publication' => 'Publish!',
+	'c_accueil_publication_texte' => 'Pour publier une page, il vous faudra créer un article.
+		Il est nécessaire pour cela de créer au moins une rubrique. Vous pouvez le faire dans le menu « Édition » en cliquant « Rubriques ».', # NEW
+	'c_accueil_texte' => 'You have just entered SPIP\'s private area.',
+	'c_accueil_texte_revenir' => '
+		Cette page présente l\'activité éditoriale récente sur votre site.
+		Vous pouvez revenir sur cette page à tout instant en cliquant l\'icône de maison,
+		sur la partie haute, sous votre nom.', # NEW
+	'c_article_redaction' => 'L\'article est en cours de rédaction', # The editing of this article is in progress
+	'c_article_redaction_redacteur' => 'L\'article est en cours de rédaction', # NEW
+	'c_article_redaction_redacteur_texte' => 'Pour proposer votre article aux administrateurs du site et aux autres rédacteurs,
+		dans le cadre sur le côté, changez « en cours de rédaction » par « proposé à la publication ».', # NEW
+	'c_article_redaction_texte' => 'Pour publier cet article sur le site public, il faut changer son statut.
+		Dans le cadre sur le côté, changez « en cours de rédaction » par « publié en ligne ».', # NEW
+	'c_articles_creer' => 'How to create an article?',
+	'c_articles_creer_texte' => 'You cannot create an article from this page
+		unless there is at least one section in your site.
+		You can create it from the "Sections" menu under "Edit". ',
+	'c_job' => 'Les travaux à faire…', # NEW
+	'c_job_texte' => 'Cette page liste les prochaines tâches de maintenance
+		que doit effectuer SPIP. Ces tâches sont exécutées à intervalle régulier
+		ou ponctuellement pour des traitements lourds demandés par des plugins,
+		tel que l\'envoi d\'email en masse.', # NEW
+	'c_rubrique_publier' => 'Create an article',
+	'c_rubrique_publier_texte' => 'Une rubrique n\'est visible sur le site public
+		qu\'à partir du moment où elle contient au moins un contenu publié ; par exemple : un article publié.
+		Créez donc un article. Vous pouvez le faire depuis cette page, sous le descriptif de votre rubrique.', # NEW
+	'c_rubriques_creer' => 'Créez une première rubrique !', # NEW
+	'c_rubriques_creer_texte' => 'Les rubriques sont la structure de base du site ; vous pourrez créer des articles dans chacune d\'entre elles.
+		Commencez par créer une première rubrique.', # NEW
+
+	// E
+	'explication_activer_compagnon' => 'Le compagnon ajoute des commentaires sur certaines pages
+			de l\'espace privé pour aider à prendre SPIP en main. Souhaitez-vous l\'activer ?', # NEW
+	'explication_reinitialiser_compagnon' => 'Les messages déjà vus par un auteur ne sont plus affichés ensuite.
+			Souhaitez-vous réinitialiser ces messages ?', # NEW
+
+	// L
+	'label_activer_compagnon' => 'Enable the Companion?',
+	'label_reinitialiser_compagnon' => 'Reset the Companion\'s messages?',
+
+	// O
+	'ok' => 'OK',
+	'ok_bien' => 'Good!',
+	'ok_jai_compris' => 'I got it!',
+	'ok_merci' => 'Cheers',
+	'ok_parfait' => 'Alright!',
+
+	// R
+	'reinitialisation' => 'Resetting',
+	'reinitialisation_ok' => 'Resetting done',
+	'reinitialiser' => 'Reset',
+	'reinitialiser_moi' => 'Yes, only those I\'ve already read',
+	'reinitialiser_tous' => 'Yes, whoever is the author',
+
+	// T
+	'titre_compagnon' => 'The Companion',
+	'titre_page_configurer_compagnon' => 'Configure the companion'
+);
+
+?>
