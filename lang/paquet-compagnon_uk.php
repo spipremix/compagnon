5bd738d80c8150b4d0ee94bfeb1ec46a3fa992d3
olly.olga <olly.olga@gmail.com>
2015-05-06 03:33:08 +0000

    [Salvatore] [source:_core_/plugins/compagnon/lang/ paquet-compagnon] Export depuis http://trad.spip.net de la langue uk

---
+++ lang/paquet-compagnon_uk.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/paquet-compagnon?lang_cible=uk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compagnon_description' => 'Плагін виводить довідку про систему управління при першому вході користувача до адміністративної частини.',
+	'compagnon_nom' => 'Помічник (Companion)',
+	'compagnon_slogan' => 'Початкова допомога при роботі зі SPIP'
+);
+
+?>
