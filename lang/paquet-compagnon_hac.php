35bd0a2b681fd54bf565630a4b147e3e4b4fcbd2
ako <ako@mail.com>
2017-12-06 04:35:32 +0000

    [Salvatore] [source:_core_/plugins/compagnon/lang/ paquet-compagnon] Export depuis http://trad.spip.net de la langue hac

---
+++ lang/paquet-compagnon_hac.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-compagnon?lang_cible=hac
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compagnon_description' => 'کەسی هامڕا چە یەکەمین سەردانیکەردەو نۋیسنگاو پەشتۊSPIP .خزمەتو بەکاربەرا کەرۊ',
+	'compagnon_nom' => 'ھامڕا',
+	'compagnon_slogan' => 'ڕانیشاندەرو یەکەمین ھەنگامەکا چنی SPIP'
+);
