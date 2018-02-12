2face39cfacec69696c2ef642718eb2185d994c5
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-12 04:36:06 +0000

    [Salvatore] [source:_core_/plugins/compagnon/lang/ paquet-compagnon] Export depuis http://trad.spip.net de la langue br

---
+++ lang/paquet-compagnon_br.php
@@ -0,0 +1,17 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-compagnon?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compagnon_description' => 'An ambrouger a ginnig sikour d’an implijerien
+		da vare ar c’hentañ weladenn el lec’h prevez SPIP.',
+	'compagnon_nom' => 'Ambrouger',
+	'compagnon_slogan' => 'Ambrouger evit kregiñ gant SPIP'
+);
