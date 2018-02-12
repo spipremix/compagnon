552f660dce4642519996ec0fdcc71bd54c44ec31
nolwenn.berbey <nolwenn.berbey@gmail.com>
2018-02-12 04:36:04 +0000

    [Salvatore] [source:_core_/plugins/compagnon/lang/ compagnon] Export depuis http://trad.spip.net de la langue br

--- lang/compagnon_br.php
+++ lang/compagnon_br.php
@@ -10,39 +10,54 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// C
-	'c_accueil_bienvenue' => 'digemer mad @nom@',
-	'c_accueil_configurer_site' => 'Frammañ ho lec’hien', # RELIRE
-	'c_accueil_publication' => 'embann',
-	'c_accueil_publication_texte' => 'Evit embann ur bajenn e tleoc’h sevel ur pennad.
-		Dav eo sevel ur rubrikenn d’an nebeutañ evit-se.  Gallout a rit sevel unan e lañser "Embann" en ur klikañ war "rubrikennoù".', # RELIRE
-	'c_accueil_texte_revenir' => '		Ar pajenn-mañ a ginnig ar pennadoù diwezhañ ho peus embannet war ho lec’hienn.
-		Gallout a rit dont en-dro d’ar pajenn-mañ pep wech ho peus c’hoant en ur klikañ war ar skeuden "ti" a-hus, dindan ho anv.
-		', # RELIRE
-	'c_article_redaction' => 'Ar pennad-mañ zo c’hoazh e skrivañ', # RELIRE
-	'c_article_redaction_redacteur' => 'Ar pennad-mañ zo c’hoazh e skrivañ', # RELIRE
-	'c_article_redaction_texte' => 'Evit embann ar pennad-mañ war al lec’hienn foran eo dav cheñj  e statud dezhañ.
-		E lañser disachañ e tu gleiz lakait "embannet enlinenn" e plas " e skrivañ".', # RELIRE
-	'c_articles_creer' => 'penaos krouin ur pennad skrid', # RELIRE
-	'c_job' => 'labourioù da ober…',
-	'c_rubrique_publier' => 'krouit un embann skrid',
-	'c_rubriques_creer' => 'embannit ur rubrik gentañ !',
-	'c_sites_creer' => 'penaos krouiñ pe emporzhiñ ul lec’h?',
+	'c_accueil_bienvenue' => 'Degemer mat @nom@',
+	'c_accueil_configurer_site' => 'Kefluniañ ho lec’h',
+	'c_accueil_publication' => 'Embannit !',
+	'c_accueil_publication_texte' => 'Evit embann ur bajenn e tleoc’h sevel ur pennad-skrid.
+		Ret eo sevel ur rubrikenn d’an nebeutañ evit-se. Gallout a rit sevel unan e lañser "Embann" en ur klikañ war "Rubrikennoù".',
+	'c_accueil_texte' => 'E lec’h prevez SPIP emaoc’h bremañ.',
+	'c_accueil_texte_revenir' => '		Ar bajenn-mañ a ginnig ar pennadoù diwezhañ ho peus embannet war ho lec’h.
+		Gallout a rit dont en-dro d’ar bajenn-mañ pep wech ho peus c’hoant en ur klikañ war ar skeuden "ti" a-hus, dindan hoc’h anv.
+		',
+	'c_article_redaction' => 'Ar pennad-mañ zo war-ober da skrivañ',
+	'c_article_redaction_redacteur' => 'Ar pennad-mañ zo war-ober da skrivañ',
+	'c_article_redaction_texte' => 'Evit embann ar pennad-mañ war al lec’h publik ret eo cheñch e statud.
+		Er stern a-gostez chenchit "war-dro da skrivañ" en "embannet enlinenn".',
+	'c_articles_creer' => 'Penaos krouiñ ur pennad-skrid ?',
+	'c_articles_creer_texte' => 'Ne C’hallit ket krouiñ ur pennad-skrid er pajenn-mañ
+		ma n’eus rubrikenn ebet en ho lec’h.
+		Gallout a rit krouiñ anezhi el lañser "Embann", ha goude "Rubrikennoù" .',
+	'c_job' => 'Labourioù da ober…',
+	'c_rubrique_publier' => 'Krouit un embann skrid',
+	'c_rubriques_creer' => 'Embannit ur rubrikenn gentañ !',
+	'c_rubriques_creer_texte' => 'Framm al lec’h eo ar rubrikennoù ; galllout a rit krouiñ pennadoù-skrid e pep unan.
+		Kinnig a rit krouiñ ur c’hentañ rubrikenn.',
+	'c_sites_creer' => 'Penaos krouiñ pe enporzhiañ ul lec’h ?',
+	'c_sites_creer_texte' => 'Ne c’hallit ket krouiñ pe enporzhiñ ul lec’h abaoe ar bajenn-mañ ma n’eus rubrikenn ebet en ho lec’h.
+		Gallout a rit krouiñ anezhi el lañser "Embann", ha goude "Rubrikennoù".',
+
+	// E
+	'explication_activer_compagnon' => 'Ouzhpennañ a ra an ambrouger evezhiadennoù er pajennoù al lec’h prevez evit sikour ac’hanoc’h o implijout SPIP. Ezhomm hoc’h eus gweredekaet anezhañ ?',
 
 	// L
-	'label_reinitialiser_compagnon' => 'adreizhañ kannad ar c’hanfard ?',
+	'label_activer_compagnon' => 'Gweredekaat an ambrouger ?',
+	'label_reinitialiser_compagnon' => 'Adreizhañ kannadoù an ambrouger ?',
 
 	// O
-	'ok_bien' => 'mad eo',
-	'ok_jai_compris' => 'komprenned  meus',
-	'ok_merci' => 'trugarez',
-	'ok_parfait' => 'mat tre',
+	'ok' => 'Mat eo',
+	'ok_bien' => 'Mat eo !',
+	'ok_jai_compris' => 'komprennet em meus !',
+	'ok_merci' => 'Trugarez',
+	'ok_parfait' => 'Mat tre !',
 
 	// R
-	'reinitialisation_ok' => 'ya nemedken re  meus dija lennet .',
-	'reinitialiser' => 'adreizhet',
-	'reinitialiser_moi' => 'ya nemedken re meus dija lennet',
-	'reinitialiser_tous' => 'ya, sort pe sort vefe an aoser',
+	'reinitialisation' => 'Adkoradur',
+	'reinitialisation_ok' => 'Echu eo an adkoradur.',
+	'reinitialiser' => 'Adreizhet',
+	'reinitialiser_moi' => 'Ya nemedken re meus dija lennet',
+	'reinitialiser_tous' => 'Ya, sort pe sort vefe an aoser',
 
 	// T
-	'titre_compagnon' => 'ar genderw'
+	'titre_compagnon' => 'An Ambrouger',
+	'titre_page_configurer_compagnon' => 'Kefluniañ an Ambrouger'
 );
