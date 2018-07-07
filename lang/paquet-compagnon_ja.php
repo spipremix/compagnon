74fd630ffe956f90ddc119d6b80e960758aa546b
origenius <origenius@mvb.biglobe.ne.jp>
2018-07-07 03:39:18 +0000

    [Salvatore] [source:_core_/plugins/compagnon/lang/ paquet-compagnon] Export depuis http://trad.spip.net de la langue ja

---
+++ lang/paquet-compagnon_ja.php
@@ -0,0 +1,16 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-compagnon?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'compagnon_description' => '初めてSPIPの管理エリアをご訪問する時、仲間さんは、援助を提供してくださいます。',
+	'compagnon_nom' => '仲間さん',
+	'compagnon_slogan' => 'SPIPを使用するように、最初のステップウィザードです。'
+);
