cbf65662b6480d8bd7d55dc494bc0dfce680045f
origenius <origenius@mvb.biglobe.ne.jp>
2018-07-07 03:39:16 +0000

    [Salvatore] [source:_core_/plugins/compagnon/lang/ compagnon] Export depuis http://trad.spip.net de la langue ja

---
+++ lang/compagnon_ja.php
@@ -0,0 +1,44 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/compagnon?lang_cible=ja
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// C
+	'c_accueil_bienvenue' => '@nom@ようこそ！',
+	'c_accueil_configurer_site' => 'サイト環境を設定する',
+	'c_accueil_publication' => '公開してください！',
+	'c_accueil_texte' => 'SPIPの管理エリアに入りました。',
+	'c_article_redaction' => '記事は、編集中です。',
+	'c_article_redaction_redacteur' => '記事は、編集中です。',
+	'c_articles_creer' => 'どうやって記事を作成しましょうか。',
+	'c_job' => 'するべき務め…',
+	'c_rubrique_publier' => '記事を作成してください。',
+	'c_rubriques_creer' => '第一のセクションを作成してください！',
+	'c_sites_creer' => 'サイトを作成またはインポートする方法は？',
+
+	// L
+	'label_activer_compagnon' => '「仲間さん」を有効にしますか。',
+	'label_reinitialiser_compagnon' => '仲間さんのメッセージをリセットしますか。',
+
+	// O
+	'ok' => 'オケー',
+	'ok_bien' => 'よかったです！',
+	'ok_jai_compris' => '了解しました！',
+	'ok_merci' => 'ありがとう！',
+	'ok_parfait' => '最高！',
+
+	// R
+	'reinitialisation' => 'リセット',
+	'reinitialisation_ok' => 'リセットは完全になりました。',
+	'reinitialiser' => 'リセットする',
+
+	// T
+	'titre_compagnon' => '仲間さん',
+	'titre_page_configurer_compagnon' => '「仲間さん」を設定する'
+);
